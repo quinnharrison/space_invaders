d224a8480f72c0b6e3dbc6dd382f2068e6cb0d2d
Quinn Harrison <m182472@usna.edu>
2018-02-15 11:35:03 -0500

    added asteroid class

---
+++ quinn/quinn_asteroid.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import pygame
+from colors import *
+
+
+class Asteroid(#insert sprite):
+	#create asteroid object and give it initial position and velocity
+	def __init__(self):
+          self.x = random.randint(10,390)
+          self.y = 0
+          self.vy = random.randint(1,10)
+	
+	#create how asteroid will move with time
+	def move(self,time):
+	  # Move in y direction
+          self.y = self.y + self.vy*time
+
+
+	#draw asteroid on surface
+	def draw(self, surf):
+	  self.asteroid = pygame.Rect((0,0,10,10))
+          self.asteroid.center=(self.x,self.y)
+          pygame.draw.rect(surf,(ASTEROID_COLOR),self.asteroid)
