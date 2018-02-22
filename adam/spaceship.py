3bcff44ff295f9cd8c0a0b5b618523ade46d0035
Adam Pressel <m185244@usna.edu>
2018-02-21 21:40:47 -0500

    werking with s m e l l y austin

--- adam/spaceship.py
+++ adam/spaceship.py
@@ -5,23 +5,23 @@ from colors import *
 
 class Spaceship(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__()
+        super().__init__() #call the parent class contructor
         self.Vx = 0
         self.Vy = 0
-        self.X = 200 #initial position
-        self.Y = 629 #initial postiion
-        self.image = pygame.image.load('spaceship.png')
-        self.image = 
-    def move(self):
-        #TODO
-        pass
+        self.image = pygame.image.load('spaceship.png') #creates surface
+        self.image.set_colorkey(BLACK) #set 'colorkey' of surface, this lets the black parts of the picture be transparent
+        self.rect = self.image.get_rect()
+        self.rect.center = (200, 629) #initializes center of the spaceship
+    def move(self, time):
+        self.rect.move(self.Vx*time, -1*self.Vy*time)
+        
     def isHit(self):
         #TODO
         return false
     def fire(self):
         #TODO
         pass
-    def draw(self):
-        #TODO
-        pass
+    def draw(self, surf):
+        #draw image on surface
+        surf.blit(self.image, self.rect)
     
